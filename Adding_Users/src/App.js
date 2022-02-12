365ebb130475f9597cc8f3a302be29d64c1252de
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-02-12 13:29:41 -0700

    added components.  Started with a User comonent with a form and inputs. Added a card component to style the overall inputs.  added styling via css module.  injected classes intot the card from the card-style sheet and the adduser styles component.

--- Adding_Users/src/App.js
+++ Adding_Users/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import AddUser from './compononents/Users/AddUser.component'
 
 function App() {
   return (
     <div>
-
+      
+        <AddUser />
+      
     </div>
   );
 }
