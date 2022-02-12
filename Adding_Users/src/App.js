8efb20270e3f8166031e2ab721414177e917fc8f
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-02-12 13:53:26 -0700

    added button component.  added style modules for button component.  made the type dependent on the button being used.  also gave button click functionality.

--- Adding_Users/src/App.js
+++ Adding_Users/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AddUser from './compononents/Users/AddUser.component'
+import AddUser from './compononents/Users/AddUser/AddUser.component'
 
 function App() {
   return (
--- Adding_Users/src/compononents/Users/AddUser/AddUser.component.js
+++ Adding_Users/src/compononents/Users/AddUser/AddUser.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Card from '../UI/Card.component'
-
+import Card from '../../UI/Card/Card.component'
+import Button from '../../UI/Button/Button.component'
 import classes from './AddUser-styles.module.css'
 
 const AddUser = (props) => {
@@ -16,7 +16,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label id="username">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
     </form>
     </Card>
     )
