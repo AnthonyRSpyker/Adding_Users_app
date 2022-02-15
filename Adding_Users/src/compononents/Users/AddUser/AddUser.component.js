3ddf4fbb81d563d5af60f8a53559bb4b55c744cb
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-02-15 11:27:53 -0700

    added a simple validation that checks if input fields are filled. If they are not, nothing happens.  Also checks to see if age is larger than 0.

--- Adding_Users/src/compononents/Users/AddUser/AddUser.component.js
+++ Adding_Users/src/compononents/Users/AddUser/AddUser.component.js
@@ -11,6 +11,14 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault()
+
+        if(enteredName.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+        if(+enteredAge < 1){
+            return;
+        }
+
         console.log(enteredAge, enteredName)
         setEnteredName('')
         setEnteredAge('')
