b16124f3f1a3c5f3a6a4e66518688082c58d24b6
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-02-17 12:11:21 -0700

    alternatively used React.Fragment to wrap the App.js return statement.  An alternative to the wrapper component work around

--- Adding_Users/src/App.js
+++ Adding_Users/src/App.js
@@ -13,12 +13,12 @@ function App() {
   }
 
   return (
-    <div>
+    <React.Fragment>
       
         <AddUser onAddUser={addUserHandler} />
         <UserList users={usersList} />
       
-    </div>
+    </React.Fragment>
   );
 }
 
